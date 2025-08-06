878b2025495ca8c7459bbafc1927a639536665c6
FreireCF <clecioferreira011@gmail.com>
2025-08-06 12:21:30 -0300

    Adicionando função de limpar o input

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let nomes = [];
 
+function limparCampo(){
+  let campo = document.querySelector('input');
+  campo.value = '';
+}
+
 function adicionarAmigo(){
   let nome = document.getElementById('amigo').value;
   nomes.push(nome);
+  limparCampo();
   console.log(nomes);
 }
